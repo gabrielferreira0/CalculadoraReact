35425837ced01318dd4a9d4a50578c129a13585d
Gabriel Ferreira <47874487+gabrielferreira0@users.noreply.github.com>
2021-01-28 19:07:32 -0300

    Buttons add

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from './Main/Calculator';
 import './index.css'
-import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
     <React.StrictMode>
@@ -12,4 +12,4 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
-reportWebVitals();
+
