5bcef73298d0e49c6e221dc226fbccbea6162030
Gabriel Ferreira <47874487+gabrielferreira0@users.noreply.github.com>
2021-02-03 19:46:51 -0300

    Projeto finalizado

--- src/Main/Calculator.jsx
+++ src/Main/Calculator.jsx
@@ -23,14 +23,30 @@ export default class Calculator extends Component {
     }
 
     clearMemory() {
-        //console.log('limpar')
-        this.setState({initialState})
+        this.setState({displayValue: '0'})
     }
 
     setOperation(operation) {
         console.log(operation)
+        if (this.state.current == 0) {
+            this.setState({operation: operation, current: 1, clearDisplay: true})
+        } else {
+            const result = operation === '='
+            const currentOperation = this.state.operation
+            const values = [...this.state.values]
+            values[0] = eval(`${values[0]}
+            ${currentOperation}
+            ${values[1]}`)
+            values[1] = 0;
+            this.setState({
+                displayValue: values[0],
+                operation: result ? null : operation,
+                current: result ? 0 : 1,
+                clearDisplay: !result,
+                values
+            })
+        }
     }
-
     addDigit(n) {
         //console.log(n);
         if (n === '.' && this.state.displayValue.includes('.')) {
@@ -40,9 +56,17 @@ export default class Calculator extends Component {
             || this.state.clearDisplay
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
-        this.setState({displayValue, clearDisplay: false})
-    }
+        this.setState({displayValue: displayValue, clearDisplay: false})
 
+        if (n !== '.') {
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[i] = newValue;
+            this.setState({values})
+            console.log(values);
+        }
+    }
     render() {
         return (
             <div className='calculator'>
